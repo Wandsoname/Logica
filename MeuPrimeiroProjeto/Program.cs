6f47a7f20cccead360957a0836a0b76369240689
wandson <wandsoname@gmail.com>
2026-05-13 21:35:32 -0300

    Segunda Versão

--- MeuPrimeiroProjeto/Program.cs
+++ MeuPrimeiroProjeto/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Informe seu nome: ");
+string nome = Console.ReadLine()!;
+
+Console.WriteLine("Informe sua idade: ");
+int idade = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Informe sua altura: ");
+double altura = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Possui CNH? (true/false) ");
+bool cnh = bool.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Meu nome é: " + nome);
+Console.WriteLine("Minha idade é: " + idade);
+Console.WriteLine("Minha altura é: " + altura);
+Console.WriteLine(cnh);
